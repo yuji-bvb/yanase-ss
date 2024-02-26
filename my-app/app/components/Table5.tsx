55912494ed2b9ef794f2c9e1ec242c9b4ba174aa
Yuji yanase <yuji.bvb@gmail.com>
2024-02-26 16:03:09 +0900

    add table

---
+++ my-app/app/components/Table5.tsx
@@ -0,0 +1,39 @@
+export default function Table5() {
+  return (
+    <div className="flex flex-col p-5 italic">
+      <div className="-m-1.5 overflow-x-auto">
+        <div className="p-1.5 min-w-full inline-block align-middle">
+          <div className="overflow-hidden">
+            <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+              <caption className="py-2 text-center text-sm sm:text-2xl text-gray-800 dark:text-gray-800 font-bold">
+                その他設備、施設
+              </caption>
+              <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                <tr>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm sm:text-2xl font-medium text-gray-800 dark:text-gray-800">
+                    金型製品保管倉庫
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm sm:text-2xl text-gray-800 dark:text-gray-800"></td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm sm:text-2xl text-gray-800 dark:text-gray-800">
+                    2棟
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm sm:text-2xl font-medium text-gray-800 dark:text-gray-800">
+                    フォークリフト
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm sm:text-2xl text-gray-800 dark:text-gray-800">
+                    　　　　
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm sm:text-2xl text-gray-800 dark:text-gray-800">
+                    1台
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -4,24 +4,25 @@ import Table1 from "./components/Table1";
 import Table2 from "./components/Table2";
 import Table3 from "./components/Table3";
 import Table4 from "./components/Table4";
-
+import Table5 from "./components/Table5";
 
 export default function Home() {
   return (
     <div>
       <Image
-          className="relative"
-          src="/press.jpg"
-          alt="Next.js Logo"
-          width={1500}
-          height={300}
-          priority
-        />
-        <Header/>
-        <Table1/>
-        <Table2/>
-        <Table3/>
-        <Table4/>
+        className="relative"
+        src="/press2.jpg"
+        alt="Next.js Logo"
+        width={1500}
+        height={300}
+        priority
+      />
+      <Header />
+      <Table1 />
+      <Table2 />
+      <Table3 />
+      <Table4 />
+      <Table5 />
     </div>
   );
 }
