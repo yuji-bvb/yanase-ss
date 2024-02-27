cbbaf8f5feba57bf4fd4ba35537611a567155a20
Yuji yanase <yuji.bvb@gmail.com>
2024-02-27 09:25:04 +0900

    change freq

--- my-app/app/sitemap.ts
+++ my-app/app/sitemap.ts
@@ -1,36 +1,36 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'http://localhost:3000/',
+      url: "http://localhost:3000/",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: 'http://localhost:3000/corporate',
+      url: "http://localhost:3000/corporate",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.8,
     },
     {
-      url: 'http://localhost:3000/contact',
+      url: "http://localhost:3000/contact",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.5,
     },
     {
-        url: 'http://localhost:3000/library',
-        lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 0.5,
-      },
-      {
-        url: 'http://localhost:3000/recruit',
-        lastModified: new Date(),
-        changeFrequency: 'yearly',
-        priority: 0.5,
-      },
-  ]
+      url: "http://localhost:3000/library",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+    {
+      url: "http://localhost:3000/recruit",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
 }
