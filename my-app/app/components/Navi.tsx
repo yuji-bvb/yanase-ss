54661b7945fda649faa5c819c35fc2678c7a4200
Yuji yanase <yuji.bvb@gmail.com>
2024-03-15 17:33:54 +0900

    change navbar

--- my-app/app/components/Navi.tsx
+++ my-app/app/components/Navi.tsx
@@ -1,85 +1,103 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function NavBar() {
+  const [isClick, setisClick] = useState(false);
+
+  const toggleNavbar = () => {
+    setisClick(!isClick);
+  };
   return (
-    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm py-3 sm:py-0">
-      <nav
-        className="relative max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
-        aria-label="Global"
-      >
-        <div className="flex items-center justify-between">
-          <Link
-            className="flex-none text-3xl font-bold"
-            href="/"
-            aria-label="Brand"
-          >
-            (有)簗瀬製作所
-          </Link>
-          <div className="sm:hidden">
-            <button
-              type="button"
-              className="hs-collapse-toggle w-9 h-9 flex justify-center items-center gap-x-2 text-sm text-xl font-semibold rounded-lg border border-white/20 border-white/40 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-              data-hs-collapse="#navbar-collapse-with-animation"
-              aria-controls="navbar-collapse-with-animation"
-              aria-label="Toggle navigation"
-            >
-              <svg
-                className="hs-collapse-open:hidden flex-shrink-0 w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+    <header>
+      <nav>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <a className="text-3xl font-bold" href="/">
+                  (有)簗瀬製作所
+                </a>
+              </div>
+            </div>
+            <div className="hidden md:block">
+              <div className="ml-4 flex items-center space-x-4">
+                <Link className="text-xl font-semibold p-2" href="/corporate">
+                  会社概要
+                </Link>
+                <Link className="text-xl font-semibold p-2" href="/contact">
+                  お問い合わせ
+                </Link>
+                <Link className="text-xl font-semibold p-2" href="/recruit">
+                  採用情報
+                </Link>
+                <Link className="text-xl font-semibold p-2" href="/library">
+                  ライブラリ
+                </Link>
+              </div>
+            </div>
+            <div className="md:hidden flex items-center">
+              <button
+                className="inline-flex items-center justify-center p-2 rounded-md"
+                onClick={toggleNavbar}
               >
-                <line x1="3" x2="21" y1="6" y2="6" />
-                <line x1="3" x2="21" y1="12" y2="12" />
-                <line x1="3" x2="21" y1="18" y2="18" />
-              </svg>
-              <svg
-                className="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
-            </button>
+                {isClick ? (
+                  <svg
+                    className="flex-shrink-0 w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <path d="M18 6 6 18" />
+                    <path d="m6 6 12 12" />
+                  </svg>
+                ) : (
+                  <svg
+                    className="flex-shrink-0 w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="3" x2="21" y1="6" y2="6" />
+                    <line x1="3" x2="21" y1="12" y2="12" />
+                    <line x1="3" x2="21" y1="18" y2="18" />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
         </div>
-        <div
-          id="navbar-collapse-with-animation"
-          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
-        >
-          <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7">
-            <a
-              className="text-xl font-semibold   sm:py-6"
-              href="/corporate"
-              aria-current="page"
-            >
-              会社概要
-            </a>
-            <a className="text-xl font-semibold /[.8] sm:py-6" href="contact">
-              お問い合わせ
-            </a>
-            <a className="text-xl font-semibold /[.8] sm:py-6" href="recruit">
-              採用情報
-            </a>
-            <a className="text-xl font-semibold /[.8] sm:py-6" href="library">
-              ライブラリ
-            </a>
+        {isClick && (
+          <div className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <a className="text-xl block font-semibold p-2" href="/corporate">
+                会社概要
+              </a>
+              <a className="text-xl block font-semibold p-2" href="/contact">
+                お問い合わせ
+              </a>
+              <a className="text-xl block font-semibold p-2" href="/recruit">
+                採用情報
+              </a>
+              <a className="text-xl block font-semibold p-2" href="/library">
+                ライブラリ
+              </a>
+            </div>
           </div>
-        </div>
+        )}
       </nav>
     </header>
   );
