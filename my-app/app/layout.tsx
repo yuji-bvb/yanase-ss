386b8e43ae190c3b036f230b0729c77d64b5c744
Yuji yanase <yuji.bvb@gmail.com>
2024-03-17 16:27:09 +0900

    add SEO

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -7,10 +7,32 @@ import Footer from "./components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const siteName = "簗瀬製作所";
+const description =
+  "簗瀬製作所の公式ホームページです。プレス加工による自動車部品等の製造及び溶接加工を行っています。";
+const url = "https://yanase-seisakusho.com/";
+
 export const metadata: Metadata = {
-  title: "簗瀬製作所",
-  description:
-    "簗瀬製作所の公式ホームページです。プレス加工による自動車部品等の製造及び溶接加工を行っています。",
+  title: {
+    default: siteName,
+    template: `%s - ${siteName}`,
+  },
+  description,
+  openGraph: {
+    title: siteName,
+    description,
+    url,
+    siteName,
+    locale: "ja_JP",
+    type: "website",
+  },
+  verification: {
+    google:
+      "google-site-verification=0sTZZXgYlBKNKGvHcYgzIh8bCzhrsXWoh_dwbOeUXpA",
+  },
+  alternates: {
+    canonical: url,
+  },
 };
 
 export default function RootLayout({
